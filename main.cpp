4a53c225d3ee34d7e5057f9ac3a63189ae4a6ce6
nyebe-14 <ramirezian037@gmail.com>
2023-12-18 10:38:57 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int Add2Values(int, int);
+
+int main()
+{
+    int x, y;
+    cout
+        << "-- Add 2 Values --\n"
+        << "Enter x Values: ";
+    cin >> x;
+    cout << "Enter y Value: ";
+    cin >> y;
+
+    cout << "__________________";
+
+    cout << Add2Values(x, y)
+
+    return 0;
+}
+
+int Add2Values(int x, int y)
+{
+    // make your code here
+}
